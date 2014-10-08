1229b7672f13350bcb4109105fa7d23f7412cc0b
Kenton Newby <kentonnewby@gmail.com>
2014-10-07 21:18:11 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "memcached-chef"
+maintainer        "Mix Digital Media, LLC"
+maintainer_email  "info@mixdigitalmedia.com"
+description       "Simple memcached installation"
+version           "0.0.1"
+
+recipe "look_and_feel-chef", "Installs memcached"
+
+supports "ubuntu"
